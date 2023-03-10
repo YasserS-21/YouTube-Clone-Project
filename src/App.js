d51e1844c0295132d6769c2a3c58fbc5d96cc044
YasserS-21 <saadi.yasser04@gmail.com>
2023-03-10 11:23:03 -0500

    Added Router/Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Nav from './Components/Nav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './Components/About';
+import VideosIndex from './Components/VideosIndex';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<VideosIndex/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
