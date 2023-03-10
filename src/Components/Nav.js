d489caae75f48ceb8b12c109a2dcb03b91deaaf1
Wilton <wiltoncamilol@gmail.com>
2023-03-10 12:22:20 -0500

    Changes to Nav and Nav.css

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,24 @@
-export default function Nav () {
-    return(
-        <>
-        <h1>Random shit</h1>
-        </>
-    )
+import { Link } from "react-router-dom"
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <header>
+      <article>
+        <h1>
+          Youtube
+        </h1>
+      </article>
+      <nav>
+        <ul>
+          <li>
+           <p>Home</p>
+          </li>
+          <li>
+            <p>About</p>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
