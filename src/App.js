e529682b30998e743e45d88be6b7717837bf9a3a
Ryan Lundy <rlundy1792@gmail.com>
2023-03-13 15:38:44 -0400

    Modal Window on 400 error

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from './Components/About';
 import VideosIndex from './Components/VideosIndex';
 import VideoPlayer from './Components/VideoPlayer';
+import Modal from './Components/Modal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Nav/>
-      <Routes>
-        <Route path="/" element={<VideosIndex/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/watch/:videoId" element={<VideoPlayer/>}/>
-      </Routes>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<VideosIndex />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/watch/:videoId" element={<VideoPlayer />} />
+          <Route path="/error" element={< Modal />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/Components/VideosIndex.js
+++ src/Components/VideosIndex.js
@@ -2,42 +2,89 @@ import { useState, useEffect } from "react";
 import { getVideos } from "../api/fetch";
 import YouTube from "react-youtube";
 import VideoListing from "./VideoListing";
+import Modal from "./Modal";
+import { useNavigate } from "react-router-dom";
 
 const key = process.env.REACT_APP_API_KEY;
 
 
-export default function VideosIndex () {
+export default function VideosIndex() {
     const [search, setSearch] = useState("")
     const [videos, setVideos] = useState([])
+    const [errorMessage, setError] = useState(null)
+    const [modal, setModal] = useState(false);
+    // const navigate = useNavigate();
+
     function searchVideos(e) {
         e.preventDefault()
         setSearch(e.target[0].value)
         console.log(videos)
     }
+
+
+
     useEffect(() => {
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&type=video&key=${key}`)
-        .then((response) => response.json())
-        .then((response)=>{
-            setVideos(response.items)
-        })
-    },[search]);
-  
+        const handleErrors = (response) => {
+            if (!response.ok) {
+                if (response.status === 400) {
+                    response.text().then((text) => setError(text));
+                    // navigate("/error")
+                    setModal(true);
+                }
+            }
+            return response;
+        };
+
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${search}&key=${key}`)
+
+            .then(handleErrors)
+
+            .then((response) => {
+                if (response.status === 400) {
+                    setError({
+                        code: response.status,
+                        message: "The server was unable to process the request due to invalid information sent by the client. Please modify your request and try again"
+                    })
+                    setModal(!modal)
+                } else {
+                    return response.json()
+                }
+            })
+            .then((response) => {
+                setVideos(response.items)
+                setError(false);
+            })
+            .catch((error) => {
+                console.log(error)
+                setError({
+                    code: "Error",
+                    message: "An error occurred while fetching the videos. Please try again later."
+                })
+                // navigate("/error")
+                setModal(!modal)
+            })
+    }, [search]);
+
     return (
         <>
-        <div className="searchBar">
-        <form className="searchForm" onSubmit={searchVideos}>
-        <input type="text"></input>
-        <button>Search</button>
-        </form>
-        </div>
-        
-        <div className="Videos">
-            <ul>
-            {search? videos.map((video) => {
-            return <VideoListing video={video}/>
-            }) : <p>no videos yet fam</p>}
-            </ul>
-        </div>
+            <div className="searchBar">
+                <form className="searchForm" onSubmit={searchVideos}>
+                    <input type="text"></input>
+                    <button>Search</button>
+                </form>
+            </div>
+
+            <div className="Videos">
+                {errorMessage ? (
+                    <Modal code={errorMessage.code} message={errorMessage.message} modal={modal} setModal={setModal} />
+                ) : (
+                    <ul>
+                        {search ? videos.map((video) => {
+                            return <VideoListing video={video} />
+                        }) : <p>no videos yet fam</p>}
+                    </ul>
+                )}
+            </div>
         </>
     )
 }
