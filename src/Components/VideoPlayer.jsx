5f30bf55808ad8319226cb46218d1da45eb84d33
YasserS-21 <saadi.yasser04@gmail.com>
2023-03-14 13:27:11 -0400

    Deleted Extra Files

--- src/Components/VideoPlayer.jsx
+++
@@ -1,9 +0,0 @@
-import YouTube from "react-youtube"
-export default function VideoPlayer () {
-
-    return(
-        <>
-        <YouTube videoId={`${(window.location.href).split("/")[4]}`}/>
-        </>
-    )
-}
